eb3bfa8a8b67182c6d435c2bc01ac36d71d8f92f
michelemichetti <michele.michetti91@gmail.com>
2024-07-14 09:08:22 +0000

    Autoformat code

--- game/src/Tile.cpp
+++ game/src/Tile.cpp
@@ -3,7 +3,6 @@
 #include <Tile.hpp>
 #include <utils.hpp>
 
-
 Tile::Tile(uint8_t collision_directions_bitmask, uint8_t interaction_directions_bitmask) {
     this->collision_directions_bitmask_ = collision_directions_bitmask;
     this->interaction_directions_bitmask_ = interaction_directions_bitmask;
