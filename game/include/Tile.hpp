aed61fced5b62577e131d2a81fcda90daa142268
Michele Michetti <michele.michetti91@gmail.com>
2024-07-14 12:33:06 +0200

    Updated Tile.*

--- game/include/Tile.hpp
+++ game/include/Tile.hpp
@@ -11,8 +11,6 @@ class Tile {
     bool isCollision(utils::Direction player_direction);
     bool isInteraction(utils::Direction player_direction);
 
-    utils::Coordinate getCoordinate();
-
    private:
     uint8_t collision_directions_bitmask_;
     uint8_t interaction_directions_bitmask_;
--- game/src/Tile.cpp
+++ game/src/Tile.cpp
@@ -3,9 +3,10 @@
 #include <Tile.hpp>
 #include <utils.hpp>
 
-Tile::Tile(uint8_t collision_directions_bitmask, uint8_t interaction_directions_bitmask) {
+Tile::Tile(uint8_t collision_directions_bitmask, uint8_t interaction_directions_bitmask, InteractionTile interaction) {
     this->collision_directions_bitmask_ = collision_directions_bitmask;
     this->interaction_directions_bitmask_ = interaction_directions_bitmask;
+    this->interaction = interaction;
 }
 
 Tile::~Tile() {}
