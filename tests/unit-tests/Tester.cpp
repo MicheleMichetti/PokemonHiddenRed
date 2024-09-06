c09dbb05c5d1e9106be4c75f846abfd39741e0c6
michelemichetti <michele.michetti91@gmail.com>
2024-09-06 22:06:49 +0200

    Created Tester.cpp and ut_Tile.hpp

--- tests/unit-tests/Tester.cpp
+++ tests/unit-tests/Tester.cpp
@@ -1,11 +1,7 @@
-#include <Map.hpp>
+#include "ut_Tile.hpp"
 
-#include "ut_Tile.cpp"
 
-int main() {
-    collision(utils::Direction::up);
-    EXPECT_TRUE(collision(utils::Direction::down));
-    EXPECT_FALSE(collision(utils::Direction::left));
-    EXPECT_TRUE(collision(utils::Direction::right));
-    EXPECT_FALSE(collision(utils::Direction::none));
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- tests/unit-tests/ut_Tile.hpp
+++ tests/unit-tests/ut_Tile.hpp
@@ -11,10 +11,27 @@ bool collision(uint8_t direction) {
     return testTile.isCollision(direction);
 }
 
+bool interaction(uint8_t direction) {
+    InteractionTile interTile;
+    Tile testTile(0b00001010, 0b00000101, interTile);
+
+    std::cout << "Test Interaction " << direction << std::endl;
+
+    return testTile.isInteraction(direction);
+}
+
 TEST(collision, Positive) {
     EXPECT_FALSE(collision(utils::Direction::up));
     EXPECT_TRUE(collision(utils::Direction::down));
     EXPECT_FALSE(collision(utils::Direction::left));
     EXPECT_TRUE(collision(utils::Direction::right));
     EXPECT_FALSE(collision(utils::Direction::none));
+}
+
+TEST(interaction, Positive) {
+    EXPECT_TRUE(interaction(utils::Direction::up));
+    EXPECT_FALSE(interaction(utils::Direction::down));
+    EXPECT_TRUE(interaction(utils::Direction::left));
+    EXPECT_FALSE(interaction(utils::Direction::right));
+    EXPECT_FALSE(interaction(utils::Direction::none));
 }
