134600c371cb650e22b6e2c77df90c229eb8e306
Michele Michetti <92488318+MicheleMichetti@users.noreply.github.com>
2024-10-11 09:36:13 +0000

    Autoformat code

--- game/src/BinaryfileRW.cpp
+++ game/src/BinaryfileRW.cpp
@@ -1,15 +1,15 @@
-#include<BinaryFileRW.hpp>    
+#include <BinaryFileRW.hpp>
 
-BinaryFileRW::BinaryFileRW(std::string filename, std::ofstream ouput_stream){
+BinaryFileRW::BinaryFileRW(std::string filename, std::ofstream ouput_stream) {
     File output_file(filename);
     this->output_stream = output_file.read;
 }
-    BinaryFileRW::BinaryFileRW(std::string filename,std::ifstream input_stream){}
-    BinaryFileRW::BinaryFileRW(std::string filename,std::ofstream ouput_stream, std::ifstream input_stream){}
-    ~BinaryFileRW::BinaryFileRW(){}
-    template <class T>
-    T BinaryFileRW::readBitsAtPosition(uint64_t position){}
-    template <class T, class Z>
-    void BinaryFileRW::writeBitsAtPosition(T target, Z bitmask, uint64_t position){}
-    template <class T, class Z>
-    void BinaryFileRW::writeBitsInPipeline(T target, Z bitmask){}
+BinaryFileRW::BinaryFileRW(std::string filename, std::ifstream input_stream) {}
+BinaryFileRW::BinaryFileRW(std::string filename, std::ofstream ouput_stream, std::ifstream input_stream) {}
+~BinaryFileRW::BinaryFileRW() {}
+template <class T>
+T BinaryFileRW::readBitsAtPosition(uint64_t position) {}
+template <class T, class Z>
+void BinaryFileRW::writeBitsAtPosition(T target, Z bitmask, uint64_t position) {}
+template <class T, class Z>
+void BinaryFileRW::writeBitsInPipeline(T target, Z bitmask) {}
