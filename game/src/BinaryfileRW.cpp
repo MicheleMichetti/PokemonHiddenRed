a7dda895f75dc674019750efc0c1c754bfcc832e
Michele Michetti <92488318+MicheleMichetti@users.noreply.github.com>
2024-10-11 11:35:55 +0200

    Pseudocode for first constructor

---
+++ game/src/BinaryfileRW.cpp
@@ -0,0 +1,15 @@
+#include<BinaryFileRW.hpp>    
+
+BinaryFileRW::BinaryFileRW(std::string filename, std::ofstream ouput_stream){
+    File output_file(filename);
+    this->output_stream = output_file.read;
+}
+    BinaryFileRW::BinaryFileRW(std::string filename,std::ifstream input_stream){}
+    BinaryFileRW::BinaryFileRW(std::string filename,std::ofstream ouput_stream, std::ifstream input_stream){}
+    ~BinaryFileRW::BinaryFileRW(){}
+    template <class T>
+    T BinaryFileRW::readBitsAtPosition(uint64_t position){}
+    template <class T, class Z>
+    void BinaryFileRW::writeBitsAtPosition(T target, Z bitmask, uint64_t position){}
+    template <class T, class Z>
+    void BinaryFileRW::writeBitsInPipeline(T target, Z bitmask){}
